9a5ff425f3c6b78ad2e63025e5dcb73070b3e8d7
soundarya444 <42862894+soundarya444@users.noreply.github.com>
2019-02-08 11:41:53 +0530

    Create   fifth.py

---
+++ fifth.py
@@ -0,0 +1,13 @@
+aaa=input()
+aaa=aaa.split()
+num1=int(aaa[0])
+num2=int(aaa[1])
+num3=int(aaa[2])
+if (num1 >= num2) and (num1 > num3):
+   largest = num1
+elif (num2 >= num1) and (num2 > num3):
+   largest = num2
+else:
+   largest = num3
+ 
+print(largest
