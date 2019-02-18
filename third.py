f6217f7e0521e31128ca1f39f0028322a3f0bb17
soundarya444 <42862894+soundarya444@users.noreply.github.com>
2019-02-18 09:57:42 +0530

    third.py

---
+++ third.py
@@ -0,0 +1,7 @@
+x=str(raw_input())
+if(x in('a','e','i','o','u')):
+  print("Vowel")
+elif(x in range('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z')):
+  print("Consonant")
+else:
+  print("invalid")
